f0e7648c4466eba33d34a09d453c878ed505e856
Max <MaxLopez0029@gmail.com>
2018-12-18 16:12:20 +0800

    添加了两个页面

--- SpringBootProject/src/main/java/com/demo/demo/Service/ProjectService.java
+++ SpringBootProject/src/main/java/com/demo/demo/Service/ProjectService.java
@@ -3,6 +3,9 @@ package com.demo.demo.Service;
 import com.demo.demo.Dao.ProjectDao;
 import com.demo.demo.Entity.Project;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 /**
@@ -19,8 +22,16 @@ public class ProjectService {
     public void saveProject(Project project){
         projectDao.save(project);
     }
-
-    /**
-     *
-     */
+    
+    // 只查询没有删除的图书
+    public Page<Project> findAll(Pageable pageable) {
+    	Example<Project> example = getNotDeleteExample();
+    	return projectDao.findAll(example, pageable);
+    }
+    
+    private Example<Project> getNotDeleteExample() {
+        Project project = new Project();
+        project.setState(2);
+        return Example.of(project);
+    }
 }
